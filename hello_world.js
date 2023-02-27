8efc60a75e175bc29f513f45418020d105e7b3ad
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-02-26 23:24:24 -0500

    Edit files

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+let message = prompt("What's the official name of Javascript?");
+
+if (message = "ECMAScript") {
+  console.log("Right!");
+} else {
+  console.log("You don't know? ECMAScript!");
+}
