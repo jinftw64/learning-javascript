0c54ca42d9b1c62a16e9f7ae9fea9ccd5ec9a88e
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-13 23:14:11 -0400

    Complete DOM method and node selectors exercise

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+// this line looks for an element with id container and 
+// assigs it to const container
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
 content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+content.style.cssText = 'border: solid black; background-color: pink;';
+
+const h1 = document.createElement('h');
+h1.classList.add('content');
+h1.textContent = "I'm in a div";
+content.appendChild(h1);
+
+const p = document.createElement('p');
+p.classList.add('content');
+p.textContent = 'ME TOO!';
+content.appendChild(p);
 
 container.appendChild(content);
